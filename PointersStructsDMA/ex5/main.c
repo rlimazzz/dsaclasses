3947fdcf7191c645f2a9958184a81b1bfa9c1811
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-11-02 11:34:21 -0300

    Create main.c

---
+++ PointersStructsDMA/ex5/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    printf("Tamanho do char: %lu bytes\n", sizeof(char));
+    printf("Tamanho do inteiro: %lu bytes\n", sizeof(int));
+    printf("Tamanho do float: %lu bytes\n", sizeof(float));
+    printf("Tamanho do double: %lu bytes\n", sizeof(double));
+
+    return 0;
+}
